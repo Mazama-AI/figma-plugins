a15730c9ae94c18a81e85de4a8b398cf011fa28c
Jonathan Lederman <jlederman8@gmail.com>
2022-09-16 14:10:40 -0700

    Create plugin that generate 10x10 figma "frames"
    CU-3j2y0ph

---
+++ create-frames/code.ts
@@ -0,0 +1,26 @@
+// This file holds the main code for the plugin. It has access to the *document*.
+// You can access browser APIs such as the network by creating a UI which contains
+// a full browser environment (see documentation).
+
+// Runs this code if the plugin is run in Figma
+// This plugin creates 5 rectangles on the screen.
+const numberOfFrames = 10;
+
+const nodes: SceneNode[] = [];
+for (let i = 0; i < numberOfFrames; i++) {
+  for (let j = 0; j < numberOfFrames; j++) {
+    const frame = figma.createFrame();
+    frame.x = i * 150;
+    frame.y = j * 150;
+    frame.fills = [{ type: "SOLID", color: { r: 1, g: 0.5, b: 0 } }];
+    figma.currentPage.appendChild(frame);
+    nodes.push(frame);
+  }
+}
+figma.currentPage.selection = nodes;
+figma.viewport.scrollAndZoomIntoView(nodes);
+
+// Make sure to close the plugin when you're done. Otherwise the plugin will
+// keep running, which shows the cancel button at the bottom of the screen.
+figma.closePlugin();
+
