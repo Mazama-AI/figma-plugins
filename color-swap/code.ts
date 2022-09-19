99445d7cc4df7855ba0cb85ff3fd586aedb4720a
Jonathan Lederman <jlederman8@gmail.com>
2022-09-19 14:50:50 -0700

    wip

---
+++ color-swap/code.ts
@@ -0,0 +1,40 @@
+// This file holds the main code for the plugin. It has access to the *document*.
+// You can access browser APIs such as the network by creating a UI which contains
+// a full browser environment (see documentation).
+
+// Runs this code if the plugin is run in Figma
+// This plugin creates 5 rectangles on the screen.
+const numberOfFrames = 10;
+
+
+const nodes: SceneNode[] = [];
+const generateFrames = () => {
+  if (nodes.length > 0) {
+    console.log(nodes)
+    return;
+  } else {
+    for (let i = 0; i < numberOfFrames; i++) {
+      for (let j = 0; j < numberOfFrames; j++) {
+        let R = Math.random()
+        let G = Math.random()
+        let B = Math.random()
+        const frame = figma.createFrame();
+        frame.x = i * 150;
+        frame.y = j * 150;
+        frame.fills = [{ type: "SOLID", color: { r: R, g: G, b: B } }];
+        frame.fills
+        figma.currentPage.appendChild(frame);
+        nodes.push(frame);
+      }
+    }
+  }
+}
+generateFrames();
+
+figma.currentPage.selection = nodes;
+figma.viewport.scrollAndZoomIntoView(nodes);
+
+// Make sure to close the plugin when you're done. Otherwise the plugin will
+// keep running, which shows the cancel button at the bottom of the screen.
+figma.closePlugin();
+
